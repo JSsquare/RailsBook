676545ae798bcc900823077cdd6e5d92b5ec52e1
Johnny <johnny@johnny-Presario-C700-PC.(none)>
2011-11-06 14:47:24 +0530

    Toggle the comment area

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
     $('#user_nav').hide();
     $('#toggle_nav_side').hide();
     $('#rails_image').hide();
+    $('#comment_area').hide();
+    
       $('a#show_nav_down').click(function(){          
         $('#toggle_nav').hide();  
         $('#user_nav').toggle('slow');
@@ -16,5 +18,11 @@ $(document).ready(function(){
         $('#user_nav').toggle('slow');
         $('#rails_image').toggle('slow');
         $('#toggle_nav').fadeIn('slow');
+      });
+      
+      $('a#toggle_comment').click(function(){          
+        $('#comment_area').toggle('slow');
+        $("html, body").animate({ scrollTop: 1000 }, "slow");
+        
       });
   });
